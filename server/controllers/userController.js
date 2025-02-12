5441e081ce184ee3605430ff4311dd436824b984
AndreeHonoree <iradonoree@gmail.com>
2025-02-12 17:29:24 +0300

    User sign in feature

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,10 @@ import { createUser, findUser } from "../database/queries";
 import { requestResponseFormatter } from "../helpers/responseFormater.js";
 import * as codes from "../constants/status_codes.js";
 import bcrypt from "bcryptjs/dist/bcrypt.js";
+import { Op } from "sequelize";
+import jwt from "jsonwebtoken";
+require("dotenv").config({ path: __dirname + "/../../.env" });
+
 
 const hashPassword = async function (password) {
   const saltRounds = 10;
@@ -63,3 +67,57 @@ export const createUserController = async (req, res) => {
     );
   }
 };
+
+
+export const userSignIn = async (req, res) => {
+  const { usernameOrEmail, password  } = req.body;
+  try {
+    if(usernameOrEmail && password){
+      // Check if a user with the given email or username exists
+      const { type } = await findUser({ where:{
+        [Op.or]: [{ username: usernameOrEmail }, { email: usernameOrEmail }]}});
+      if (!type) {
+        res.status(codes.NOT_FOUND).send(
+          requestResponseFormatter("error", {
+          code: codes.NOT_FOUND,
+          message: "User not found",})
+        ); 
+      } 
+      else{
+       const passwordIsValid = await bcrypt.compareSync(password, type.password);
+        if(passwordIsValid){
+          const token = jwt.sign({ id: type.id }, process.env.JWT_SECRET, {
+            expiresIn: "1h"});
+          res.status(codes.OK).send(
+            requestResponseFormatter("error", {
+            code: codes.OK,
+            message: "User logged in successfully", token})
+          );
+        }
+        else{
+          res.status(codes.UNAUTHORIZED).send(
+            requestResponseFormatter("error", {
+            code: codes.UNAUTHORIZED,
+            message: "Please enter your email and password",})
+          );
+        }
+      };
+    }
+    else{
+      res.status(codes.BAD_REQUEST).send(
+        requestResponseFormatter("error", {
+        code: codes.BAD_REQUEST,
+        message: "Sign up to create an account",})
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(codes.INTERNAL_SERVER_ERROR).send(
+      requestResponseFormatter("error", {
+        code: codes.INTERNAL_SERVER_ERROR,
+        message: `Something went wrong ${error.message}`,
+      })
+    );
+  }
+};
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,8 @@
-import { createUserController, checkAndCreateUser } from "../controllers/userController.js";
+import { createUserController, checkAndCreateUser, userSignIn} from "../controllers/userController.js";
 import { Router } from "express";
 const router = Router();
 
 router.post("/signup", checkAndCreateUser, createUserController);
+router.post("/signin", userSignIn);
 
 export { router };
